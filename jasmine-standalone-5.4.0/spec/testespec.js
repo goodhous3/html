5d91857b0acc1886248c2728046d797c5ecbda4f
Karino <124396383+goodhous3@users.noreply.github.com>
2024-11-08 16:13:40 -0300

    Add files via upload

---
+++ jasmine-standalone-5.4.0/spec/testespec.js
@@ -0,0 +1,77 @@
+//NO TESTESPEC.JS VAI FICAR O TESTE
+
+// ex. 1
+// describe("Multiplicação ", function(){
+//     it("Resultado: ", function(){
+
+//         let a = 2;
+//         let b = 4;
+//         let c = 6;
+        
+//         expect(multiplicacao(a, b, c)).toBe(48);
+//     })
+// }
+// )
+
+// ex. 2
+// describe ("Divisão: ", function(){
+//     it("Resultado: ", function(){
+
+
+//         expect(divisao(6,2)).toBe(3)
+//     })
+  
+// })
+
+//ex. 3 
+// describe("Celsius para Fahrenheit:", function(){
+//     it("Resultado: ", function(){
+
+//         expect(celsiusFahrenheit(0)).toBe(32)
+//     })
+// })
+
+// describe("Fahrenheit para Celsius:", function(){
+//     it("Resultado: ", function(){
+
+//         expect(fahrenheitCelsius(32)).toBe(0)
+//     })
+// })
+
+
+//ex.4 
+// describe("Calcular Média: ", function(){
+//     it("Resultado: ", function(){
+
+//         expect(calcularMedia(10,20,30)).toBe(20)
+//     })
+// })
+
+//ex.5
+// describe("Contar Caracteres: ", function(){
+//     it("Resultado:", function(){
+//         expect(contarCaracteres("amar")).toBe(4)
+//     })
+// })
+
+//ex.6
+// describe("Operações:", function(){
+//     it("Soma: ", function(){
+//         expect(operacoes(10, 20, "+")).toBe(30)
+//     })
+
+//     it("Subtração: ", function(){
+//         expect(operacoes(10, 10, "-")).toBe(0)
+//     })
+
+//     it("Multiplicação: ", function(){
+//         expect(operacoes(10, 20, "*")).toBe(200)
+//     })
+
+//     it("Divisão: ", function(){
+//         expect(operacoes(10, 5, "/")).toBe(2)
+//     })
+
+// })
+
+
---
+++ jasmine-standalone-5.4.0/src/teste.js
@@ -0,0 +1,69 @@
+//teste.js aqui vamos colocar o código fonte
+
+
+
+//ex. 1
+// function multiplicacao(a, b, c){
+  
+//     let multiplicacao = a * b * c;
+
+//     return multiplicacao;
+// }
+
+//ex. 2 
+// function divisao(a, b){
+
+//     let divisao = a / b;
+
+//     if(b == 0){
+//         return "Não é possível dividir por zero"
+//     } else {
+//         return divisao;
+//     }
+   
+// }
+
+// ex.3
+// function celsiusFahrenheit(c){
+
+//     let celsius = (c * 1.8) + 32
+
+//     return celsius;
+// }
+
+// function fahrenheitCelsius(f){
+
+//     let fahrenheit = (f - 32)/1.8
+
+//     return fahrenheit;
+// }
+
+
+// ex.4
+// function calcularMedia(a, b, c){
+//     let media = (a + b + c) / 3;
+//     return media;
+// }
+
+//ex.5
+// function contarCaracteres(){
+//     let frase = "amar";
+//     let caracteres = frase.length;
+//     return caracteres;
+// }
+
+//ex.6 
+// function operacoes(a,b,c){
+//     let soma = a + b;
+//     let sub = a - b;
+//     let mult = a * b;
+//     let div = a / b;
+
+//     switch (c) {
+//         case "+": return soma
+//         case "-": return sub
+//         case "*": return mult
+//         case "/": return div
+//     }
+// }
+
